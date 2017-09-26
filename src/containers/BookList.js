3ae132924201260bf34e355d7a9662810430964e
Devon Moubry <devon@moubry.com>
2017-09-26 14:04:32 -0500

    Show list of books

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { selectBook }  from '../actions/index';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectBook } from "../actions/index";
 //make sure action created flows through all reducers
-import { bindActionCreators } from 'redux';
-
-
+import { bindActionCreators } from "redux";
 
 class BookList extends Component {
   render() {
-
-    //must create a map function here to return the following:
-
-
-    // ******************
-    //
-    //     <li
-    //       key={book.id}
-    //       onClick={() => this.props.selectBook(book)}
-    //       className="list-group-item">{book.title}</li>
-    //
-    // ******************
+    let books = this.props.books;
+    //map returns list of books with onClick
+    let List = books.map(book => {
+      return (
+        <li
+          key={book.id}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
+      )
+    });
 
     return (
       <ul className="list-group col-sm-4">
         {/* return your mapped array list items here */}
+        {List}
       </ul>
     );
   }
@@ -34,7 +33,7 @@ function mapStateToProps(state) {
   //what is returned will show up as props inside of BookList
   //this gives you access to books in props.. (books would be good for mapping)
   return {
-    books: state.books,
+    books: state.books
   };
 }
 
@@ -43,7 +42,7 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   //whenever selectBook is called, result should be passed to
   //all of the reducers. (flows through dispatch function -- like a funnel)
-    return bindActionCreators({ selectBook: selectBook }, dispatch)
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
 }
 
 //connect all functions to container component
